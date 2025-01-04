5a11a9e2f1c10fb601ceab7dffe5e25b2a22dd7a
Fa-Amine <farah@gmail.com>
2025-01-04 14:50:30 +0100

    user entity (db 3arfo fin ghaytpusha)

--- src/main/java/com/springdata/frauddetectioniaauthservice/Entities/User.java
+++ src/main/java/com/springdata/frauddetectioniaauthservice/Entities/User.java
@@ -1,4 +1,81 @@
 package com.springdata.frauddetectioniaauthservice.Entities;
 
+
+import jakarta.persistence.*;
+
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(unique=true)
+    private String email;
+    @Column
+    private String password;
+    @Column
+    private String firstName;
+    @Column
+    private String lastName;
+    @Column
+    private String role;
+
+    public User() {
+    }
+
+    public User(Long id, String email, String password, String firstName, String lastName, String role) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.role = role;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
